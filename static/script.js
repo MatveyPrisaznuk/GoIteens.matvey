0e2acc2b987c0645bd39a2ca27560f98daec623c
MatveyPrisaznuk <matveyprys@gmail.com>
2023-10-17 21:03:38 +0300

    comment

---
+++ static/script.js
@@ -0,0 +1,14 @@
+/*function changeColorAndImage() {
+    var image = document.getElementById("myImage");
+    var colors = ["red", "green", "blue", "orange", "purple"];
+    var currentColor = image.style.backgroundColor || "white"; // Початковий колір або білий, якщо не вказано
+
+    // Змінюємо колір на випадковий інший, якщо є більше кольорів
+    var newColor = colors[Math.floor(Math.random() * colors.length)];
+    while (newColor === currentColor) {
+        newColor = colors[Math.floor(Math.random() * colors.length)];
+    }
+
+    image.style.backgroundColor = newColor;
+}
+*/
